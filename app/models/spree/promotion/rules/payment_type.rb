9b1995b7e9dfd4ad5eeebcfacfb48b0b0ec2458c
Quintin Adam <quintinadam@live.com>
2015-05-26 10:26:25 -0600

    adds join table migration

--- app/models/spree/promotion/rules/payment_type.rb
+++ app/models/spree/promotion/rules/payment_type.rb
@@ -2,6 +2,7 @@ module Spree
   class Promotion
     module Rules
       class PaymentType < Spree::PromotionRule
+        has_and_belongs_to_many :payment_methods, class_name: '::Spree::Payment', join_table: 'spree_payment_methods_promotion_rules', foreign_key: 'promotion_rule_id'
         def applicable?(promotable)
           promotable.is_a?(Spree::Order)
           raise 'the roof'
---
+++ db/migrate/20150526162346_create_spree_payment_methods_promotion_rules.rb
@@ -0,0 +1,8 @@
+class CreateSpreePaymentMethodsPromotionRules < ActiveRecord::Migration
+  def change
+    create_table :spree_payment_methods_promotion_rules do |t|
+      t.references :payment_methods, index: true
+      t.references :promotion_rule, index: true
+    end
+  end
+end
