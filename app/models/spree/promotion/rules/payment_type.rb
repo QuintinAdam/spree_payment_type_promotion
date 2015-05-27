cec8e7533340ec309bed2538e209aaeb5349a4ae
Quintin Adam <quintinadam@live.com>
2015-05-27 16:06:04 -0600

    forgetting about the admin view for now

--- app/models/spree/promotion/rules/payment_type.rb
+++ app/models/spree/promotion/rules/payment_type.rb
@@ -3,9 +3,9 @@ module Spree
     module Rules
       class PaymentType < Spree::PromotionRule
         has_and_belongs_to_many :payment_methods, class_name: '::Spree::Payment', join_table: 'spree_payment_promotion_rules', foreign_key: 'promotion_rule_id'
+
         def applicable?(promotable)
           promotable.is_a?(Spree::Order)
-          raise 'the roof'
         end
 
         def eligible?(order, options = {})
