6d36955a6f2fb2af7351e1317c0456bc4b6c9b9d
Quintin Adam <quintinadam@live.com>
2015-05-28 12:15:34 -0600

    fixes the migration. setts up payment_type rule

--- app/models/spree/promotion/rules/payment_type.rb
+++ app/models/spree/promotion/rules/payment_type.rb
@@ -2,20 +2,21 @@ module Spree
   class Promotion
     module Rules
       class PaymentType < Spree::PromotionRule
-        has_and_belongs_to_many :payment_methods, class_name: '::Spree::Payment', join_table: 'spree_payment_promotion_rules', foreign_key: 'promotion_rule_id'
+        has_and_belongs_to_many :payment_methods, class_name: '::Spree::PaymentMethod', join_table: 'spree_payment_methods_promotion_rules', foreign_key: 'promotion_rule_id'
 
         def applicable?(promotable)
           promotable.is_a?(Spree::Order)
         end
 
         def eligible?(order, options = {})
+          # raise 'the roof'
           if order.payments.any?
-            raise 'the roof'
+            promotion.promotion_rules.where(type: "Spree::Promotion::Rules::PaymentType").first.payment_methods.any?{|pay_type| order.payments.include?(pay_type) }
           end
         end
 
         def actionable?(line_item)
-          raise 'the roof'
+          true
         end
       end
     end
--- db/migrate/20150526162346_create_spree_payment_methods_promotion_rules.rb
+++ db/migrate/20150526162346_create_spree_payment_methods_promotion_rules.rb
@@ -1,8 +1,10 @@
 class CreateSpreePaymentMethodsPromotionRules < ActiveRecord::Migration
   def change
-    create_table :spree_payment_promotion_rules do |t|
-      t.references :payment_methods, index: true
-      t.references :promotion_rule, index: true
+    create_table :spree_payment_methods_promotion_rules, id: false, force: true do |t|
+      t.references :payment_method
+      t.references :promotion_rule
     end
+    add_index :spree_payment_methods_promotion_rules, [:payment_method_id], name: 'index_payment_methods_promotion_rules_on_payment_method_id'
+    add_index :spree_payment_methods_promotion_rules, [:promotion_rule_id], name: 'index_payment_methods_promotion_rules_on_promotion_rule_id'
   end
 end
